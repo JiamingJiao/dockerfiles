c89abcc7c7edb5fc97c142c8840158366fc4816c
JiamingJiao <jiaojm@outlook.com>
2025-07-16 23:22:36 +0800

    vscode server path

--- cuda12.4-cudnn9-vscode1.102.0/build.sh
+++ cuda12.4-cudnn9-vscode1.102.0/build.sh
@@ -1,2 +1,3 @@
 # For docker desktop on windows
+# 设置desktop代理，wsl环境变量，按照片设置wsl网络（不要mirrored）
 docker build -t jiaojiaming/deeplearning:cuda12.4-cudnn-vscode1.102.0 --build-arg "HTTP_PROXY=http://host.docker.internal:7890" --build-arg "HTTPS_PROXY=http://host.docker.internal:7890" .
